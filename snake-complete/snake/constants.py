8b4ec5c72da620acbd4cb09894eb5a3aab926433
jayjay5032 <104041383+jayjay5032@users.noreply.github.com>
2022-06-16 15:56:10 -0600

    Add files via upload

---
+++ snake-complete/snake/constants.py
@@ -0,0 +1,16 @@
+from game.shared.color import Color
+
+
+COLUMNS = 90
+ROWS = 60
+CELL_SIZE = 10
+MAX_X = 900
+MAX_Y = 600
+FRAME_RATE = 10
+FONT_SIZE = 10
+CAPTION = "Snake"
+SNAKE_LENGTH = 8
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(0, 255, 0)
